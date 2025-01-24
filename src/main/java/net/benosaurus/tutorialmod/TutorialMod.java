64745748bf8e6adef51fd4e798c9f8fbe7584d82
Benosaurus <ben.kremer@outlook.de>
2025-01-24 22:01:46 +0100

    Added Pink Garnet and Raw Pink Garnet item

--- src/main/java/net/benosaurus/tutorialmod/TutorialMod.java
+++ src/main/java/net/benosaurus/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.benosaurus.tutorialmod;
 
+import net.benosaurus.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,5 +12,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
+		ModItems.registerModItems();
+
 	}
 }
