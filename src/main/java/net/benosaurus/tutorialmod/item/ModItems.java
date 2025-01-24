cdf837cbe97603c7692cb12a1d6a61bd4e6348ed
Benosaurus <ben.kremer@outlook.de>
2025-01-24 22:21:44 +0100

    Added Cucumber and Eggplant Item

--- src/main/java/net/benosaurus/tutorialmod/item/ModItems.java
+++ src/main/java/net/benosaurus/tutorialmod/item/ModItems.java
@@ -12,6 +12,10 @@ public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
 
+    public static final Item CUCUMBER = registerItem("cucumber", new Item(new Item.Settings()));
+    public static final Item EGGPLANT = registerItem("eggplant", new Item(new Item.Settings()));
+
+
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
     }
@@ -22,6 +26,8 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(PINK_GARNET);
             entries.add(RAW_PINK_GARNET);
+            entries.add(CUCUMBER);
+            entries.add(EGGPLANT);
         });
     }
 }
